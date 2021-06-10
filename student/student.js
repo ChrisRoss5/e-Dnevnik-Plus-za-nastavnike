2cad009fbaf3c5ef1dd11616fef49632d606e88c
Chris <kristijan.ros@gmail.com>
2021-06-10 12:46:29 +0200

    v1.2 - dodani prosjeci po kriteriju ocijenjivanja (za svaki redak u tablici prelaskom miša preko kriterija)

--- student/student.js
+++ student/student.js
@@ -4,33 +4,46 @@
   console.log("e-Dnevnik Plus za nastavnike je omogućen.");
 
   // Izračunava prosjek za tablicu ocjena u predmetu
-  let gradesTable = document.getElementById("tbl-ocjene");
+  const formatAvg = (n) => isNaN(n) ? "0,00" : n.toFixed(2).toString().replace(".", ",");
+  const gradesTable = document.getElementById("tbl-ocjene");
   if (!gradesTable) return;
-  let totalGrades = 0, gradesSum = 0;
 
-  gradesTable.querySelectorAll("td[id^='grade']").forEach((gradeBlock) => {
-    let grades = gradeBlock.textContent.match(/\d+/g);
-    if (grades) {
-      for (let i = 0; i < grades.length; i++) {
-        gradesSum += parseInt(grades[i]);
-        totalGrades++;
-      }
+  let totalGrades = 0;
+  let gradesSum = 0;
+
+  const tableRows = gradesTable.querySelectorAll("tr");
+  for (const row of tableRows) {
+    const gradesRow = row.querySelectorAll("td[id^='grade']");
+    if (!gradesRow.length) continue;
+    let rowTotalGrades = 0;
+    let rowGradesSum = 0;
+    for (const gradeBlock of gradesRow) {
+      const grades = gradeBlock.textContent.match(/\d+/g) || [];
+      rowTotalGrades += grades.length;
+      rowGradesSum += grades.map(Number).reduce((a, b) => a + b, 0);
     }
-  });
+    if (!rowTotalGrades) continue;
 
-  let avgNumber = gradesSum / totalGrades;
-  avgNumber = isNaN(avgNumber) ? "0,00" : avgNumber.toFixed(2).toString().replace(".", ",");
-  let avgTitle = "Broj ocjena: " + totalGrades + " | Zbroj ocjena: " + gradesSum;
+    // Dodavanje prosjeka po retku
+    row.firstElementChild.innerHTML += '<div class="plus-row-avg">' +
+      formatAvg(rowGradesSum / rowTotalGrades) + "</div>";
+    totalGrades += rowTotalGrades;
+    gradesSum += rowGradesSum;
+  }
+
+  const avgNumber = formatAvg(gradesSum / totalGrades);
+  const avgTitle = "Broj ocjena: " + totalGrades + " | Zbroj ocjena: " + gradesSum;
   let avg = document.getElementById("prosjek");
 
-  if (avg) {  // Prostor za prosjek već postoji
+  if (avg) {
 
+    // Prostor za prosjek već postoji
     avg.textContent = "Prosjek ocjena: " + avgNumber;
     avg.title = avgTitle;
     avg.className = "plus-avg";
+  } else {
 
-  } else {  // Izrada prostora za prosjek ispod tablice
-
+    // Izrada prostora za prosjek ispod tablice
     let old = document.getElementById("tbl-prosjek");
     old && old.remove();
     avg = document.createElement("div");
@@ -47,7 +60,5 @@
       </tbody> \
     </table>';
     gradesTable.after(avg);
-
   }
-
 })();
